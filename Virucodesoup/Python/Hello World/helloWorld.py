2e83ecccab4386cca99a013b1d5c18a9f4ab7b3d
Biolardi Yoshogi <vsio_777@yahoo.com.au>
2012-03-10 20:39:27 +0700

    fixing

---
+++ Virucodesoup/Python/Hello World/helloWorld.py
@@ -0,0 +1,16 @@
+# ===========================
+# File : helloWorld.py
+#
+# Credit:
+# - Coded by (aka in cyber) Vsio Stitched
+#
+# Misc:
+# - Written in Ruby programming language
+#
+# License:
+# - Free to use
+# - May include me or not in credit if included in other project
+# ===========================
+
+print "Hello World!";
+var = raw_input("");
