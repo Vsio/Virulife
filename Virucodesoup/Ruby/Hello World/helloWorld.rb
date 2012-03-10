05a4607b71912e638e7cbc2bd0a30185ff295a17
Biolardi Yoshogi <vsio_777@yahoo.com.au>
2012-03-10 13:06:31 +0700

    added Ruby and Hello World

---
+++ Virucodesoup/Ruby/Hello World/helloWorld.rb
@@ -0,0 +1,17 @@
+# ===========================
+# File : helloWorld.rb
+#
+# Credit:
+# - Coded by (aka in cyber) Vsio Stitched
+#
+# Misc:
+# - Written in Ruby programming language
+#
+# License:
+# - Free to use
+# - May include me or not in credit if included in other project
+# ===========================
+
+puts "Hello World!"
+gets
+
