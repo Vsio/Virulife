947b44673680bb12435dbc8120b9bfd41ababb0e
Biolardi Yoshogi <vsio_777@yahoo.com.au>
2012-04-15 10:17:54 +0700

    Function Syntax

---
+++ Virucodesoup/Ruby/Standard/Basic Syntax/Function Syntax/function.rb
@@ -0,0 +1,25 @@
+# ===========================
+# File : function.rb
+#
+# Credit:
+# - Coded by (aka in cyber) Vsio Stitched
+#
+# Misc:
+# - Written in Ruby programming language
+#
+# License:
+# - Free to use
+# - May include me or not in credit if included in other project
+# ===========================
+
+# This is to do Function block. In ruby, you can use "def". You also must put "return" so the function will return value.
+
+def add(_number1,_number2) 
+	# Do _number1+_number2
+	
+	return (_number1+_number2) # output of function
+end
+
+puts add(19,81) # prints result: 100
+
+gets # pauses command so it doesn't exit immediately
---
+++ Virucodesoup/Ruby/Standard/Basic Syntax/Function Syntax/readme.txt
@@ -0,0 +1,21 @@
+Name:
+Function Syntax
+
+About:
+- Shows how to do Function statement in Ruby
+- Written in Ruby programming language
+
+License:
+- Free to use
+- May include me or not in credit if included in other project
+
+Page:
+http://virucodesoup.blogspot.com/2012/04/ruby-function-syntax.html
+
+Files:
+- function.rb
+- readme.txt
+- run.bat
+
+Credit:
+- Coded by (aka in cyber) Vsio Stitched
