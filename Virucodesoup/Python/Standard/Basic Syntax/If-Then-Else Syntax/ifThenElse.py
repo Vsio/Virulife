42593ec53cfea3ebc99a0fc57b0284489d3114f1
Biolardi Yoshogi <vsio_777@yahoo.com.au>
2012-04-21 16:41:52 +0700

    update

---
+++ Virucodesoup/Python/Standard/Basic Syntax/If-Then-Else Syntax/ifThenElse.py
@@ -0,0 +1,26 @@
+# ===========================
+# File : ifThenElse.py
+#
+# Credit:
+# - Coded by (aka in cyber) Vsio Stitched
+#
+# Misc:
+# - Written in Python programming language
+#
+# License:
+# - Free to use
+# - May include me or not in credit if included in other project
+# ===========================
+
+# This is to simulate conditional If-Then syntax. If true, then it will executes something. If false, it will also exexute something which usually different if the condional result is true. For practice, try to change var1 and var 2 value.
+
+var1 = 5 # random value 1
+var2 = 5 # random value 2
+
+if (var1 == var2):
+	print "Value is true" # If true, then prints "Value is true"
+else:
+	print "Value is false" # If false, then prints "Value is false"
+
+
+raw_input("") # pauses command so it doesn't exit immediately
---
+++ Virucodesoup/Python/Standard/Basic Syntax/If-Then-Else Syntax/readme.txt
@@ -0,0 +1,21 @@
+Name:
+If-Then-Else Syntax
+
+About:
+- Shows how to do If-Then-Else statement in Python 
+- Written in Python programming language
+
+License:
+- Free to use
+- May include me or not in credit if included in other project
+
+Page:
+http://virucodesoup.blogspot.com/2012/04/python-if-then-else-syntax.html
+
+Files:
+- ifThenElse.py
+- readme.txt
+- run.bat
+
+Credit:
+- Coded by (aka in cyber) Vsio Stitched
